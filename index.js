3c7f7ba1a3316b1407a5f7f80e4376e698119c08
Eswar Reddy <eswarreddy7999@gmail.com>
2022-11-30 20:14:56 +0530

    ver2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express()
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/apisample', {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
+mongoose.connect('mongodb+srv://eswar:eswarreddy@cluster0.nvkcuqs.mongodb.net/test', {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
     if(err)
         console.log('Error connecting to database', err);
     else
